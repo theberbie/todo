05dda41010142888b5a5050ce7c9e9264d9d604b
vagrant <vagrant@precise32.(none)>
2016-06-25 20:29:58 +0000

    added Update

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -10,6 +10,19 @@ RSpec.describe TasksController, type: :controller do
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
     end
+
+  end
+
+    describe "tasks#update" do
+      it "should allow tasks to be marked as done" do
+        task = FactoryGirl.create(:task, done: false)
+        put :update, id: task.id, task: {done: true}
+        expect(response).to have_http_status(:success)
+        task.reload
+        expect(task.done).to eq(true)
+
+
+    end
   end
 
 end
