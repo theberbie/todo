afe16ebddcb7ec5c85d851446abd1463b6610b96
vagrant <vagrant@precise32.(none)>
2016-06-26 00:03:37 +0000

    Added Toggle options

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,10 +1,12 @@
 $(function() {
-    
-
-    
-        function taskHtml(task) {
+    // The taskHtml method takes in a JavaScript representation
+    // of the task and produces an HTML representation using
+    // <li> tags
+    function taskHtml(task) {
       var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+      var liClass = task.done ? "completed" : "";
+      var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+      '<div class="view"><input class="toggle" type="checkbox"' +
         " data-id='" + task.id + "'" +
         checkedStatus +
         '><label>' +
@@ -21,16 +23,20 @@ $(function() {
     function toggleTask(e) {
       var itemId = $(e.target).data("id");
 
-
       var doneValue = Boolean($(e.target).is(':checked'));
-
 
       $.post("/tasks/" + itemId, {
         _method: "PUT",
         task: {
           done: doneValue
         }
-      });
+      }).success(function(data) {
+        var liHtml = taskHtml(data);
+        var $li = $("#listItem-" + data.id);
+        $li.replaceWith(liHtml);
+        $('.toggle').change(toggleTask);
+
+      } );
     }
 
     $.get("/tasks").success( function( data ) {
@@ -47,8 +53,7 @@ $(function() {
     });
 
 
- 
-    $('#new-form').submit(function(event){
+    $('#new-form').submit(function(event) {
       event.preventDefault();
       var textbox = $('.new-todo');
       var payload = {
@@ -56,14 +61,12 @@ $(function() {
           title: textbox.val()
         }
       };
-      $.post("/tasks", payload).success(function(data){
+      $.post("/tasks", payload).success(function(data) {
         var htmlString = taskHtml(data);
         var ulTodos = $('.todo-list');
         ulTodos.append(htmlString);
         $('.toggle').click(toggleTask);
-
-      });
-
       });
     });
-  
+
+  });
